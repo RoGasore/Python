444c1caba08cdc370d030d5559b9789769fe75cd
RoG <123166147+RoGasore@users.noreply.github.com>
2025-08-17 03:03:42 +0200

    Create conversion vers snake case.py

---
+++ algorithmes/conversion vers snake case.py
@@ -0,0 +1,29 @@
+#Cet algo prend une chaîne écrite en CamelCase ou PascalCase et la convertit automatiquement en snake_case
+#Avec la boucle for et avec la compréhension des listes
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+# snake_cased_char_list = []
+# for char in pascal_or_camel_cased_string:
+#     if char.isupper():
+#         converted_character = '' + char.lower()
+#         snake_cased_char_list.append(converted_character)
+#     else:
+#         snake_cased_char_list.append(char)
+# snake_cased_string = ''.join(snake_cased_char_list)
+# clean_snake_cased_string = snake_cased_string.strip('')
+
+# return clean_snake_cased_string  
+
+snake_cased_char_list = [  
+    '_' + char.lower() if char.isupper()  
+    else char  
+    for char in pascal_or_camel_cased_string  
+]  
+
+return ''.join(snake_cased_char_list).strip('_')
+
+def main():
+print(convert_to_snake_case('aLongAndComplexString'))
+
+main()
+
